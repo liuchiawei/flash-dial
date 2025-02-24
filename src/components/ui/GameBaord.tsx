c41de915579894acef33d62477aac256c84f1ffe
LIU CHIAWEI <doublecheap@gmail.com>
2025-02-24 22:51:40 +0900

    special rule

--- src/components/ui/GameBaord.tsx
+++ src/components/ui/GameBaord.tsx
@@ -20,30 +20,36 @@ const GameBoard: React.FC<GameBoardProps> = ({
   return (
     <div
       className={`w-full max-w-4xl h-full grid gap-1 ${
-        size === 10 ? "grid-cols-10" : size === 7 ? "grid-cols-7" : size === 5 ? "grid-cols-5" : "grid-cols-3"
+        size === 10
+          ? "grid-cols-10"
+          : size === 7
+          ? "grid-cols-7"
+          : size === 5
+          ? "grid-cols-5"
+          : "grid-cols-3"
       }`}
     >
       {numbers.map((number) => {
         // const isTarget = targetSequence.includes(number);
-        const isClicked = targetSequence.indexOf(number) < nextExpectedIndex;
+        const isClicked =
+          targetSequence.indexOf(number) < nextExpectedIndex &&
+          targetSequence.includes(number);
         const isWrong = wrongClick === number;
 
         return (
           <button
             key={number}
             onClick={() => handleClick(number)}
-            disabled={!isPlaying}
-            className={`relative w-full h-full text-white text-lg font-bold rounded-lg flex items-center justify-center transition-all cursor-pointer active:translate-y-[1px] active:inset-shadow-sm
+            disabled={!isPlaying || !targetSequence.includes(number)}
+            className={`w-full h-full text-white text-lg font-bold rounded-lg flex items-center justify-center transition-all 
               ${
                 isClicked
                   ? "bg-teal-500"
                   : isPlaying
-                  ? "bg-gray-500"
-                  : "bg-gray-400"
+                  ? "bg-gray-600 cursor-pointer active:translate-y-[1px] active:inset-shadow-sm"
+                  : "bg-gray-500"
               }
-              ${
-                isWrong ? "bg-red-400" : ""
-              }`}
+              ${isWrong ? "bg-red-400" : ""}`}
           >
             {number}
           </button>
